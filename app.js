836833fb6063e409dbecd87b5917290feb7ddcdb
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-06-03 11:42:01 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var greetings = require('./lib.js');
+
+
+console.log(greetings.sayHello('Kim', 'Schlesinger'));
+console.log(greetings.sayGoodbye('Kim', 'Schlesinger'));
+console.log(greetings.sayAloha('Andrew', 'Jaswa'));
---
+++ lib.js
@@ -0,0 +1,11 @@
+module.exports = {
+  sayHello: function(firstName, lastName) {
+    return 'Hello, ' + firstName + ' ' + lastName + '!'
+  },
+  sayGoodbye: function(firstName, lastName) {
+    return 'Goodbye, ' + firstName + ' ' + lastName + '!'
+  },
+  sayAloha: function(firstName, lastName) {
+    return 'Aloha, ' + firstName + ' ' + lastName + '!'
+  }
+}
